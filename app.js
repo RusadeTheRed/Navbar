d9a6a6a077dd3be27c293851fc82d927def58852
Randell Anderson <zugzug07@gmail.com>
2021-09-26 10:52:36 -0500

    Functions finished

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+    // add/remove method
+//     if(links.classList.contains('show-links')){
+//         links.classList.remove('show-links')
+//     } else {
+//         links.classList.add('show-links')
+//     };
+//  toggle method
+    links.classList.toggle('show-links');
+})
